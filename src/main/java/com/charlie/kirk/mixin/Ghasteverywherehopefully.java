3b09684a83a1298af606a3ad4405de3451e4dc27
judah <judahvanwendeldejoode@gmail.com>
2025-12-23 16:48:25 +0100

    nigger

---
+++ src/main/java/com/charlie/kirk/mixin/Ghasteverywherehopefully.java
@@ -0,0 +1,16 @@
+package com.charlie.kirk.mixin;
+
+public class Ghasteverywherehopefully {
+    {
+        "type": "mincraft:desert",
+
+            "spawners": [
+        {
+            "type": "minecraft:ghast",
+                "weight": 100000,
+                "minCount": 50,
+                "maxCount": 100
+        }
+    ]
+    }
+}
