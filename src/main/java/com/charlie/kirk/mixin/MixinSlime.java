5b2509fcd2801a6255db32a601a3c7c28124b5a8
diggity <sdvdevries2@gmail.com>
2025-12-23 17:15:20 +0100

    slime big

--- src/main/java/com/charlie/kirk/mixin/MixinSlime.java
+++ src/main/java/com/charlie/kirk/mixin/MixinSlime.java
@@ -1,11 +1,48 @@
 package com.charlie.kirk.mixin;
 
+import net.minecraft.util.Mth;
+import net.minecraft.util.RandomSource;
+import net.minecraft.world.DifficultyInstance;
+import net.minecraft.world.entity.*;
+import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraft.world.entity.monster.Enemy;
 import net.minecraft.world.entity.monster.Slime;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.ServerLevelAccessor;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import javax.annotation.Nullable;
 
 @Mixin(Slime.class)
-public class MixinSlime {
-    @Shadow
+public abstract class MixinSlime extends Mob implements Enemy {
+    @Shadow(remap = false)
     public static final int MAX_NATURAL_SIZE = 10;
+
+    protected MixinSlime(EntityType<? extends Mob> entityType, Level level) {
+        super(entityType, level);
+    }
+
+
+
+    @Overwrite
+    public SpawnGroupData finalizeSpawn(ServerLevelAccessor level, DifficultyInstance difficulty, MobSpawnType spawnType, @Nullable SpawnGroupData spawnGroupData) {
+        RandomSource randomsource = level.getRandom();
+        int i = randomsource.nextInt(3);
+        if (i < 2 && randomsource.nextFloat() < 0.5F * difficulty.getSpecialMultiplier()) {
+            ++i;
+        }
+
+        int j = 1 << i;
+        this.setSize(10, true);
+        return super.finalizeSpawn(level, difficulty, spawnType, spawnGroupData);
+    }
+    @Shadow
+    public void setSize(int size, boolean resetHealth) {
+    }
+
 }
